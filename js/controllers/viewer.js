24ec1047410f7988d23cbb20d38bb1e2df56568f
Sai Medury <szm0093@auburn.edu>
2017-02-04 15:07:29 -0600

    test commit

--- js/controllers/viewer.js
+++ js/controllers/viewer.js
@@ -4,7 +4,7 @@ var app = angular.module("rover-photos");
 app.controller("viewer", ["$scope", "$http", function ($scope,$http) {
 	var ctrl = this;
 
-	$scope.photos = [
+	$scope.photos = [ 
 	{
 		 $http.get("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=erNSHrzzSFQBQmNextmpYVSf3cDX2UVp4n9c4t3A")
     .then(function(response) {
